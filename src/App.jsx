bc9a7e33e6708bd8982c6d1c1d0d73c05a9f48f1
Saiteja Challa <saitejachalla98@gmail.com>
2025-08-22 19:36:58 +0200

    Initial Commit: Create React App with Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>My React Portfolio</h1>
+      <p>This is the beginning of my new portfolio, built with React!</p>
+    </div>
+  );
+}
+
+export default App;
