01b22722c7e89e48c64bb3ba6cd1844398975ef6
Saiteja Challa <saitejachalla98@gmail.com>
2025-08-23 13:57:01 +0200

    Media in different devices resolved

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer id="contact" className="cursor-grow-target">
-      <p>Contact me at: <a href="mailto:challasaiteja26@gmail.com">challasaiteja26@gmail.com</a></p>
+      <p>Contact me at: <a className ="email-link" href="mailto:challasaiteja26@gmail.com">challasaiteja26@gmail.com</a></p>
       <p>&copy; 2025 Saiteja Challa. All Rights Reserved.</p>
     </footer>
   );
