3fe8aa96b137c5c715ed13e63f8a125c852d651a
Saiteja Challa <saitejachalla98@gmail.com>
2025-09-06 18:00:46 +0200

    No skeleton loader

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import React, { useEffect, useState } from "react"; // 1. Import useEffect
 import { useLenis } from "./hooks/useLenis"; // 2. Import Lenis
 import { ThemeProvider } from "./context/ThemeContext";
 import Header from "./components/header";
-import { Suspense } from "react";
+import About from "./components/about";
+import Projects from "./components/Projects";
+import Footer from "./components/footer";
 import CustomCursor from "./components/CustomCursor";
 import Home from "./components/Home";
-import LazyComponent from "./components/LazyComponent";
-import SkeletonLoader from "./components/SkeletonLoader";
 import { LenisProvider } from "@/context/LenisContext";
-
-const About = React.lazy(() => import("./components/about"));
-const Projects = React.lazy(() => import("./components/Projects"));
-const Footer = React.lazy(() => import("./components/footer"));
 
 function App() {
   const lenis = useLenis(); // 2. Add state to hold the Lenis instance
@@ -29,22 +25,10 @@ function App() {
         <Header />
         <Home />
         <main>
-          <Suspense fallback={<SkeletonLoader />}>
-            <LazyComponent>
-              <About />
-            </LazyComponent>
-          </Suspense>
-          <Suspense fallback={<SkeletonLoader />}>
-            <LazyComponent>
-              <Projects />
-            </LazyComponent>
-          </Suspense>
+          <About />
+          <Projects />
         </main>
-        <Suspense fallback={<div />}>
-          <LazyComponent>
-            <Footer />
-          </LazyComponent>
-        </Suspense>
+        <Footer />
       </LenisProvider>
     </ThemeProvider>
   );
